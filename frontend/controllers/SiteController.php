31a2402d7897f40b72a61cce93c74c44432af1d4
Aleksandr <rngcontrol@gmail.com>
2020-08-05 09:52:52 +0300

    17711681

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -176,7 +176,7 @@ class SiteController extends Controller
         $model = new SignupForm();
         if ($model->load(Yii::$app->request->post()) && $model->signup()) {
             //Yii::$app->session->setFlash('success', 'Thank you for registration. Please check your inbox for verification email.');
-            return $this->goHome();
+            return $this->redirect(['lk/index']);
         }
 
         return $this->render('signup', [
