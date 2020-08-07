484f7ad085b29e7452f84c12feb25b700b566a23
Aleksandr <rngcontrol@gmail.com>
2020-08-07 16:11:35 +0300

    komtet

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -1,7 +1,6 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
-    'kassa_komtet_key' => 'YbnBF5',
-    'kassa_komtet_secret' => '',
+    'kassa_komtet_key' => '',
     'kassa_komtet_queue' => '5929'
 ];
