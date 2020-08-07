0e05c033512283bf765d673068a17838da5128a3
Aleksandr <rngcontrol@gmail.com>
2020-08-07 13:03:25 +0300

    robots

--- frontend/themes/gulp/views/site/base.php
+++ frontend/themes/gulp/views/site/base.php
@@ -9,14 +9,12 @@ $this->title = Yii::$app->name;
 ?>
 
 <main class="body-container__main container content-page">
-
-      <h1>Поддерживаемые биржи</h1>
-      <ul>
-        <li><a class="link-hover" href="https://www.weblancer.net/" target="_blank" rel="nofollow">weblancer.net</a></li>
-        <li><a class="link-hover" href="https://www.fl.ru/" target="_blank" rel="nofollow">fl.ru</a></li>
-        <li><a class="link-hover" href=" https://youdo.com" target="_blank" rel="nofollow">youdo.com</a></li>
-        <li><a class="link-hover" href="https://freelance.ru/" target="_blank" rel="nofollow">freelance.ru</a></li>
-        <li><a class="link-hover" href="https://freelancehunt.com/" target="_blank" rel="nofollow">freelancehunt.com</a></li>
-      </ul>
-
+    <h1>Поддерживаемые биржи</h1>
+    <ul>
+      <li><a class="link-hover" href="https://www.weblancer.net/" target="_blank" rel="nofollow">weblancer.net</a></li>
+      <li><a class="link-hover" href="https://www.fl.ru/" target="_blank" rel="nofollow">fl.ru</a></li>
+      <li><a class="link-hover" href="https://youdo.com" target="_blank" rel="nofollow">youdo.com</a></li>
+      <li><a class="link-hover" href="https://freelance.ru/" target="_blank" rel="nofollow">freelance.ru</a></li>
+      <li><a class="link-hover" href="https://freelancehunt.com/" target="_blank" rel="nofollow">freelancehunt.com</a></li>
+    </ul>
 </main>
